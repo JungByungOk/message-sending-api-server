1ffe5fbb83b55efa0fd6cd505ccd64c8b80ee905
BOJung <jbo2541@gmail.com>
2022-10-10 21:23:19 +0900

    src commit

---
+++ src/main/java/com/example/message_send_api/MessageSendApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.message_send_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSendApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/message_send_api/MessageSendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.message_send_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
