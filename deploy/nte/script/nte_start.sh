759488e8cbd6216925e76351c079fad114cc5fbb
BOJung <jbo2541@gmail.com>
2022-11-30 16:22:12 +0900

    prod 마이그레이션

--- deploy/nte/script/nte_start.sh
+++ deploy/nte/script/nte_start.sh
@@ -1,1 +1,1 @@
-nohup java -jar /svc/nte/bin/nte.jar 1>/svc/nte/logs/stdout.log 2>/svc/nte/logs/stderr.log &
+nohup java -Dspring.profiles.active=dev -jar /svc/nte/bin/nte.jar 1>/svc/nte/logs/stdout.log 2>/svc/nte/logs/stderr.log &
